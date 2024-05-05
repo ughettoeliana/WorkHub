48f66d822296b08ba19bd8f9c4d6eb97ba7f8662
Eliana Ughetto <elianaughetto10@gmail.com>
2024-05-05 17:27:08 +0200

    added new css files & formated the documents

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
 import './App.css'
+import './assets/css/variables.css'
+import './assets/css/colors.css'
+import './assets/css/general.css'
+import './assets/css/button.css'
+
 import Footer from './components/Footer'
 import NavBar from './components/Navbar'
 import Home from './pages/Home'
