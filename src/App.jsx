d8a56c354129749a77469bf46eb5f23a2556b5ef
Eliana Ughetto <elianaughetto10@gmail.com>
2024-05-05 11:19:39 +0200

    created & added the links to the register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
 import './App.css'
 import Footer from './components/Footer'
 import NavBar from './components/Navbar'
 import Home from './pages/Home'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
 
   return (
     <>
      <NavBar/>
-     <Home />
+     <Routes>
+      <Route path='/home' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
+     </Routes>
+     
      <Footer/>
     </>
   )
