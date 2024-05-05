98d67b7183ca28146090c33341d6af163844a90c
Eliana Ughetto <elianaughetto10@gmail.com>
2024-05-05 11:18:23 +0200

    created the login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,21 @@
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <div className="container">
+      <div className="container__content">
+        <div>
+          <h1> Login</h1>
+        </div>
+        <div className="inputs__container">
+            <label htmlFor="email" className="label">Email</label>
+            <input type="email" name="email" id="email" className="input" placeholder="Email"/>
+            <label htmlFor="password" className="label">Password</label>
+            <input type="password" name="password" id="password" className="input"placeholder="Password"/>
+            <button type="submit" className="base-button submit__button">Login</button>
+            <p>Don`t have an account? <a className="link">Register</a></p>
+        </div>
+      </div>
+    </div>
+  );
+}
