6ce33c8af8556c4b72e59fb835f23094fd305f02
Eliana Ughetto <elianaughetto10@gmail.com>
2024-05-14 23:11:51 +0200

    set the variables to get the text from the inputs

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,40 @@
-import "./Register.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 
 export default function Register() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleFullNameChange = (e) => {
+    setFullName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createAccount(fullName ,email, password);
+      const loggedUser = await login(email, password);
+      localStorage.setItem("token", loggedUser.token);
+      navigate("/services", { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="login">
-      <form className="login__form">
+      <form className="login__form" action="#" onSubmit={handleFormSubmit}>
         <h1 className="login__title">Register</h1>
         <label className="login__form-label" htmlFor="fullName">
           Full name:
@@ -12,7 +43,9 @@ export default function Register() {
           className="login__form-input"
           type="text"
           name="fullName"
-          id="name"
+          id="fullName"
+          onChange={handleFullNameChange}
+          value={fullName}
           placeholder="John Doe"
         />
         <label className="login__form-label" htmlFor="email">
@@ -23,6 +56,8 @@ export default function Register() {
           type="email"
           name="email"
           id="email"
+          onChange={handleEmailChange}
+          value={email}
           placeholder="john.doe@example.com"
         />
         <label className="login__form-label" htmlFor="password">
@@ -33,6 +68,8 @@ export default function Register() {
           type="password"
           name="password"
           id="password"
+          onChange={handlePasswordChange}
+          value={password}
           placeholder="myPassword"
         />
         <label className="login__form-label" htmlFor="password">
@@ -49,7 +86,10 @@ export default function Register() {
           Register
         </button>
         <p className="wh-text-center">
-          Already have an account? <a className="wh-text-highlight">Login</a>
+          Already have an account?{" "}
+          <Link to="/login" className="wh-text-highlight">
+            Login
+          </Link>
         </p>
       </form>
     </div>
