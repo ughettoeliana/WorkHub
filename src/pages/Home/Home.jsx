b363ea82075c5614798fda7669aee83fde9d8cb9
Eliana Ughetto <elianaughetto10@gmail.com>
2024-05-14 23:11:20 +0200

    change some clases

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,27 +22,27 @@ function Home() {
 
   return (
     <div>
-      <div className="banner">
-        <h1>WorkHub</h1>
-        <p>The place where talents and opportunities come together</p>
-        <a className="base-button">Getting started</a>
+      <div className="home__banner">
+        <h1 className="home__header">WorkHub</h1>
+        <p className="home__subheader ">
+          The place where talents and opportunities come together
+        </p>
+        <a className="wh-button wh-button--primary">Getting started</a>
       </div>
-      <section className="content">
-        <h2>How does WorkHub works?</h2>
-        <div className="content__text">
-          <p>
-            WorkHub is a platform that uses detailed user profiles to match
-            people with job opportunities or clients. Our top priority is
-            honesty and integrity, so we strive to make every interaction on our
-            platform genuine and trustworthy. Feel confident and safe using our
-            platform, knowing that everything you see is real.
-          </p>
-        </div>
-        <h3>Most popular profiles</h3>
+      <section className="home__section">
+        <h2 className="home__section-header wh-text-center">How does WorkHub works?</h2>
+        <p className="home__section-description">
+          WorkHub is a platform that uses detailed user profiles to match people
+          with job opportunities or clients. Our top priority is honesty and
+          integrity, so we strive to make every interaction on our platform
+          genuine and trustworthy. Feel confident and safe using our platform,
+          knowing that everything you see is real.
+        </p>
+        <h2 className="wh-text-center wh-fs-xl">Most popular profiles</h2>
 
         <Card />
-        
-        <h2>Join us</h2>
+
+        <h2 className="wh-text-center wh-fs-xl">Join us</h2>
         <div className="promotion__container">
           <div className="promotion__banner promotion__banner-1">
             <div className="person-icon-container">
@@ -53,7 +53,7 @@ function Home() {
             </div>
             <div className="promotion__banner-text">
               <p>Create your profile and start your job searching journey </p>
-              <a className="base-button bg-Darkblue">Create profile</a>
+              <a className="wh-button wh-button--secondary">Create profile</a>
             </div>
           </div>
           <div
@@ -71,7 +71,7 @@ function Home() {
             </div>
             <div className="promotion__banner-text">
               <p>Create your profile and start your job searching journey </p>
-              <a className="base-button bg-Darkblue">Create profile</a>
+              <a className="wh-button wh-button--primary">Create profile</a>
             </div>
           </div>
         </div>
