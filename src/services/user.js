877405fee068729c26f926cdff3f651e09cc12eb
Eliana Ughetto <elianaughetto10@gmail.com>
2024-05-22 12:36:55 +0200

    created the getUser, getUserById & decodeIdToken functions

--- src/services/user.js
+++ src/services/user.js
@@ -1,5 +1,17 @@
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "./firebase";
+import { jwtDecode } from "jwt-decode";
+// import { onAuthStateChanged } from "firebase/auth";
+
+let userData = {
+  id: null,
+  email: null,
+};
 
 export async function getUsers() {
   const usersRef = collection(db, "users");
@@ -22,3 +34,62 @@ export async function getUsers() {
   }
 }
 
+export async function getUser() {
+  const idToken = localStorage.getItem('firebaseIdToken');
+  const idTokenDecoded = decodeIdToken(idToken);
+  const userId =  idTokenDecoded.user_id;
+  const userData = getUserById(userId)
+  console.log('userData', userData);
+  return userData;
+}
+
+
+export async function getUserById(id) {
+  const refUser = doc(db, `users/${id}`);
+  const docSnapshot = await getDoc(refUser);
+
+  if (docSnapshot.exists()) {
+    return {
+      id: docSnapshot.id,
+      email: docSnapshot.data().email,
+      firstName: docSnapshot.data().firstName,
+      lastName: docSnapshot.data().lastName,
+    };
+  } else {
+    console.log("No such document!");
+  }
+}
+
+
+export function decodeIdToken() {
+  const idToken = localStorage.getItem('firebaseIdToken');
+
+  if (!idToken) {
+    console.error('No ID token found in local storage');
+    return null;
+  }
+
+  try {
+    const decodedToken = jwtDecode(idToken);
+    console.log('Decoded token:', decodedToken);
+    return decodedToken;
+  } catch (error) {
+    console.error('Error decoding ID token:', error);
+    return null;
+  }
+}
+
+
+// function setUserData(newData) {
+//   userData = {
+//     ...userData,
+//     ...newData,
+//   };
+// }
+
+// function clearUserData() {
+//   setUserData({
+//     id: null,
+//     email: null,
+//   });
+// }
