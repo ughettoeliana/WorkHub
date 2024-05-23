232d47b693612a53d9718ae822aa71ab60326005
Eliana Ughetto <elianaughetto10@gmail.com>
2024-05-23 15:01:56 +0200

    added functionalities to the profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react";
+import { getUser } from "../../services/user";
+import EditUserForm from "./EditUserForm";
+
+export default function Profile() {
+  const [user, setUser] = useState({});
+  const [editMode, setEditMode] = useState(false);
+
+  function toggleEditMode() {
+    setEditMode(true);
+  }
+
+  function handleUpdateUser() {
+  }
+  
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const userData = await getUser();
+      setUser(userData);
+    }
+
+    fetchUserData();
+  }, []);
 
 
-export default function Profile () {
-  return(
+  return (
     <div>
-      User Profile
+      {user.email} :) User Profile
+      <div>
+        {!editMode && (
+          <button className="" onClick={toggleEditMode}>
+            Edit my profile
+          </button>
+        )}
+        {editMode && (
+          <EditUserForm
+            user={user}
+            handleUpdateUser={handleUpdateUser}
+            toggleEditMode={toggleEditMode}
+          />
+        )}
+      </div>
     </div>
-  )
+  );
 }
-
