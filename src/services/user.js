efe853bfce0fef6efe73798218109f3036f2c11f
Eliana Ughetto <elianaughetto10@gmail.com>
2024-06-18 18:01:32 +0200

    deleted unused functions & added updateUserData function

--- src/services/user.js
+++ src/services/user.js
@@ -1,17 +1,6 @@
-import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-} from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
 import { db } from "./firebase";
 import { jwtDecode } from "jwt-decode";
-// import { onAuthStateChanged } from "firebase/auth";
-
-// let userData = {
-//   id: null,
-//   email: null,
-// };
 
 export async function getUsers() {
   const usersRef = collection(db, "users");
@@ -35,18 +24,22 @@ export async function getUsers() {
 }
 
 export async function getUser() {
-  const idToken = localStorage.getItem('firebaseIdToken');
+  const idToken = localStorage.getItem("firebaseIdToken");
   const idTokenDecoded = decodeIdToken(idToken);
-  const userId =  idTokenDecoded.user_id;
-  const userData = getUserById(userId)
-  console.log('userData', userData);
+  const userId = idTokenDecoded.user_id;
+  const userData = await getUserById(userId);
+
+  console.log("userData", userData);
+
   return userData;
 }
-
 
 export async function getUserById(id) {
   const refUser = doc(db, `users/${id}`);
   const docSnapshot = await getDoc(refUser);
+
+  console.log("refUser", refUser);
+  console.log("docSnapshot", docSnapshot);
 
   if (docSnapshot.exists()) {
     return {
@@ -60,36 +53,35 @@ export async function getUserById(id) {
   }
 }
 
+export async function updateUserData(userId, editedUser) {
+  try {
+    const userRef = doc(db, "users", userId);
+    await updateDoc(userRef, {
+      firstName: editedUser.firstName,
+      lastName: editedUser.lastName,
+      rol: editedUser.rol,
+      address: editedUser.address,
+    });
+  } catch (error) {
+    console.error("Error al actualizar el usuario:", error);
+  }
+}
+
 
 export function decodeIdToken() {
-  const idToken = localStorage.getItem('firebaseIdToken');
+  const idToken = localStorage.getItem("firebaseIdToken");
 
   if (!idToken) {
-    console.error('No ID token found in local storage');
+    console.error("No ID token found in local storage");
     return null;
   }
 
   try {
     const decodedToken = jwtDecode(idToken);
-    console.log('Decoded token:', decodedToken);
+    console.log("Decoded token:", decodedToken);
     return decodedToken;
   } catch (error) {
-    console.error('Error decoding ID token:', error);
+    console.error("Error decoding ID token:", error);
     return null;
   }
 }
-
-
-// function setUserData(newData) {
-//   userData = {
-//     ...userData,
-//     ...newData,
-//   };
-// }
-
-// function clearUserData() {
-//   setUserData({
-//     id: null,
-//     email: null,
-//   });
-// }
