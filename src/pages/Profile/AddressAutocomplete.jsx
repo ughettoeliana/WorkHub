47faad13a2702f92689d5861d68a1ee655b94614
Eliana Ughetto <elianaughetto10@gmail.com>
2024-07-04 18:57:17 +0200

    added address api from google maps

---
+++ src/pages/Profile/AddressAutocomplete.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from 'react';
+import PropTypes from "prop-types";
+
+const AddressAutocomplete = ({ onChange, value }) => {
+  const addressInputRef = useRef(null);
+
+  useEffect(() => {
+    const autocomplete = new window.google.maps.places.Autocomplete(addressInputRef.current, {
+      types: ['address'],
+    });
+
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      onChange(place.formatted_address);
+    });
+  }, [onChange]);
+
+  return (
+    <input
+      className="login__form-input"
+      type="text"
+      name="address"
+      id="address"
+      ref={addressInputRef}
+      onChange={(e) => onChange(e.target.value)}
+      value={value}
+      required
+    />
+  );
+};
+
+
+AddressAutocomplete.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+export default AddressAutocomplete;
