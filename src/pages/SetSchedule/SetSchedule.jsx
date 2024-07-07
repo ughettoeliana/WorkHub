9caf8455ef9e595fb3db64c4e8d0be45ec38772d
Eliana Ughetto <elianaughetto10@gmail.com>
2024-07-07 21:14:59 +0200

    created setSchedule page & added a calendar component

---
+++ src/pages/SetSchedule/SetSchedule.jsx
@@ -0,0 +1,78 @@
+import Calendar from "react-calendar";
+import "./SetSchedule.css";
+import { useEffect, useState } from "react";
+import { getUserById } from "../../services/user";
+import { useNavigate, useParams } from "react-router-dom";
+
+function SetSchedule() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [user, setUser] = useState({});
+  const { userId } = useParams();
+  const [time, setTime] = useState("");
+  const navigate = useNavigate();
+
+  const handleOnclick = () => {
+    navigate("./set-schedule");
+  };
+
+  const handleTimeChange = (e) => {
+    setTime(e.target.value);
+    console.log(e.target.value);
+  };
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const userData = await getUserById(userId);
+      setUser(userData);
+    }
+    fetchUserData();
+  }, [userId]);
+
+  return (
+    <>
+      <div className="user__header">
+        <h1>
+          Ready to work with{" "}
+          <strong className="wh-color-primary">
+            {user.firstName} {user.lastName}?
+          </strong>
+        </h1>
+      </div>
+      <div className="date-time__container">
+        <div>
+          <h3>Select a date</h3>
+          <div className="calendar-wrapper">
+            <Calendar
+              onChange={setSelectedDate}
+              value={selectedDate}
+              className="custom-calendar"
+            />
+          </div>
+          {selectedDate && (
+            <p>Selected Date: {selectedDate.toLocaleDateString()}</p>
+          )}
+        </div>
+        <div className="time-container">
+          <h3>Select a time</h3>
+          <input
+            type="time"
+            className="contract__input"
+            value={time}
+            onChange={handleTimeChange}
+            step="1800" 
+          />
+          <div className="button-container">
+            <button
+              className="wh-button wh-button--primary"
+              onClick={handleOnclick}
+            >
+              Continue
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SetSchedule;
